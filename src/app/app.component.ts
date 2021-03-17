c91f0c0f9a097556ce20d6514db87d7bbeec18bf
fbedn <f_ramos@outlook.com>
2021-03-17 01:08:48 -0300

    Modificando linguagem em index.html e title em app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog-pessoal-front';
+  title = 'Front do Blog Pessoal';
 }
