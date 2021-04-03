7f512fb68d4839a5d871cac95e8f531d27684b32
fbedn <f_ramos@outlook.com>
2021-04-02 23:06:42 -0300

    implementação da publicação de postagem

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { Postagem } from '../model/Postagem';
 import { Tema } from '../model/Tema';
+import { User } from '../model/User';
 import { PostagemService } from '../service/postagem.service';
 import { TemaService } from '../service/tema.service';
 
@@ -14,8 +15,13 @@ import { TemaService } from '../service/tema.service';
 export class InicioComponent implements OnInit {
 
   postagem: Postagem = new Postagem();
-  tema: Tema = new Tema;
+
+  tema: Tema = new Tema();
   listaTemas: Tema[];
+  idTema: number;
+
+  user: User = new User();
+  idUser = environment.id;
 
   constructor(
     private router: Router,
@@ -39,8 +45,24 @@ export class InicioComponent implements OnInit {
     });
   }
 
-  publicar() {
+  findByIdTema() {
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema)=>{
+      this.tema = resp;
+    });
+  }
 
+  publicar() {
+    this.tema.id = this.idTema;
+    this.postagem.tema = this.tema;
+
+    this.user.id = this.idUser;
+    this.postagem.usuario = this.user;
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+      this.postagem = resp;
+      alert("Postagem realizada com sucesso!");
+      this.postagem = new Postagem(); //somente para zerar a postagem no front
+    });
   }
 
 }
